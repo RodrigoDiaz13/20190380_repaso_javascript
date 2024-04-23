7eef93d8c2874dbb774e624d99c3c51312098faa
RodrigoDiaz13 <108245842+RodrigoDiaz13@users.noreply.github.com>
2024-04-23 11:27:55 -0600

    ajs

--- javascript.js
+++ javascript.js
@@ -1,9 +1,9 @@
 const favoritos = [
     {
-        nombre: "Scarlett Johansson",
-        profesion: "Actriz",
-        peliculaDestacadas: "Lost in Translation", 
-        edad: 37,
+        nombre: "Jennifer Lawrence",
+        profesion: "cantante y actriz",
+        peliculaDestacadas: "The hunger games", 
+        edad: 40,
     },
     {
         nombre: "Robert Downey Jr.",
@@ -70,7 +70,7 @@ const favoritos = [
   // Función para crear una tarjeta con información de persona
   function crearTarjetaPersona(persona) {
     const tarjeta = document.createElement("div");
-    tarjeta.classList.add("bg-pink-200", "p-4", "rounded", "text-black", "m-2", "mr-10", "ml-10");
+    tarjeta.classList.add("bg-blue-200", "p-7", "rounded", "text-black", "m-2", "mr-10", "ml-10");
   
     tarjeta.innerHTML = `
           <h2 class="text-xl font-bold">${persona.nombre}</h2>
@@ -89,3 +89,19 @@ const favoritos = [
     const tarjetaPersona = crearTarjetaPersona(persona);
     contenedorTarjetas.appendChild(tarjetaPersona);
   });
+  
+// Obtener chiste de Chuck Norris y mostrar en una tarjeta
+fetch('https://rickandmortyapi.com/api/character/704')
+.then(response => response.json())
+.then(data => {
+    const aja = data.value;
+    const tarjetaaja = document.createElement("div");
+    tarjetaaja.classList.add("bg-green-200", "p-7", "rounded", "text-black", "m-2", "mr-10", "ml-10");
+
+    tarjetaaja.innerHTML = `
+        <h3>beth en traje de calamar:</h3>
+        <p>${aja}</p>
+    `;
+    contenedorTarjetas.appendChild(aja);
+})
+.catch(error => console.error('Error al obtener chiste:', error));
